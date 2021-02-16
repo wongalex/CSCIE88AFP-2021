0b0ad94e776a214ed562a75ae486c4a718fd261f
esumitra <edward.sumitra@gmail.com>
2021-02-16 00:01:43 -0500

    add resources for scala intro lecture and homework (#8)
    Co-authored-by: esumitra <esumitra@cainc.com>

---
+++ fp-scala-homework/src/main/scala/com/cscie88a/scala_intro/MyStudent.scala
@@ -0,0 +1,3 @@
+package com.cscie88a.scala_intro
+
+// add your code below
---
+++ fp-scala-homework/src/test/scala/com/cscie88a/scala_intro/HarvardStudentTest.scala
@@ -0,0 +1,14 @@
+package com.cscie88a.scala_intro
+
+import com.cscie88a.testutils.StandardTest
+
+class HarvardStudentTest extends StandardTest {
+
+  "an empty list in HarvardStudent" should {
+    val emptyList: List[String] = List.empty
+
+    "have size zero" in {
+      emptyList.size should be(0)
+    }
+  }
+}
